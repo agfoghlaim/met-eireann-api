adca6d05b8abb241bea7faba730088aecf2df03e
agfoghlaim <marieohara.gti@gmail.com>
2021-02-18 15:57:28 +0000

    Cors.

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const schema = require('./schema');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use(
   '/graphql',
+
   graphqlHttp({
     schema,
     graphiql: true,
