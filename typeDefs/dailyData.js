590b0f821b264d35f93dfe524e90d009feeb6365
agfoghlaim <marieohara.gti@gmail.com>
2021-02-19 11:18:19 +0000

    Changed dailyData types (strings that should be gql Floats). There s probably tonnes more of these.

--- typeDefs/dailyData.js
+++ typeDefs/dailyData.js
@@ -1,12 +1,12 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLFloat  } = require('graphql');
 
 const DailyDataType = new GraphQLObjectType({
   name: 'DailyData',
   fields: () => ({
     date: { type: GraphQLString, description: '00 to 00 utc' },
-    rain: { type: GraphQLString, description: 'Precipitation Amount (mm)' },
-    maxtp: { type: GraphQLString, description: 'Maximum Air Temperature (C)	' },
-    mintp: { type: GraphQLString, description: 'Minimum  Air Temperature (C)' },
+    rain: { type: GraphQLFloat, description: 'Precipitation Amount (mm)' },
+    maxtp: { type: GraphQLFloat, description: 'Maximum Air Temperature (C)	' },
+    mintp: { type: GraphQLFloat, description: 'Minimum  Air Temperature (C)' },
     gmin: {
       type: GraphQLString,
       description: '09utc Grass Minimum Temperature (C)',
@@ -14,36 +14,36 @@ const DailyDataType = new GraphQLObjectType({
     soil: {
       type: GraphQLString,
       description: 'Mean 10cm soil temperature (C)',
-    },
-    cbl: { type: GraphQLString, description: 'Mean CBL Pressure (hpa)' },
-    wdsp: { type: GraphQLString, description: 'Mean Wind Speed (kt)' },
+    }, 
+    cbl: { type: GraphQLFloat, description: 'Mean CBL Pressure (hpa)' },
+    wdsp: { type: GraphQLFloat, description: 'Mean Wind Speed (kt)' },
     hm: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Highest ten minute mean wind speed (kt)	',
     },
     ddhm: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Wind Direction at max 10 min mean (deg)',
     },
-    hg: { type: GraphQLString, description: 'Highest Gust (kt)' },
+    hg: { type: GraphQLFloat, description: 'Highest Gust (kt)' },
     pe: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Potential Evapotranspiration (mm)',
     },
-    evap: { type: GraphQLString, description: 'Evaporation (mm)' },
+    evap: { type: GraphQLFloat, description: 'Evaporation (mm)' },
     smd_wd: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Soil Moisture Deficits(mm) well drained',
     },
     smd_md: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Soil Moisture Deficits(mm) moderately drained',
     },
     smd_pd: {
-      type: GraphQLString,
+      type: GraphQLFloat,
       description: 'Soil Moisture Deficits(mm) poorly drained',
     },
-    glorad: { type: GraphQLString, description: 'Global Radiation (J/cm sq.)' },
+    glorad: { type: GraphQLFloat, description: 'Global Radiation (J/cm sq.)' },
     ind: { type: GraphQLString, description: 'Indicator (i)' },
   }),
 });
